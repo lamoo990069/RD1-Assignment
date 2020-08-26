35414081229585e3de28853dbf6372aebb6630a2
Esther-mac <esther_lin@mail.chungyo.net>
2020-08-26 10:36:05 +0800

    install moudles

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("OK");
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(80, () => {
+  console.log('Listening on port 80...');
+});
