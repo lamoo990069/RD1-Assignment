f79f04ae9b04bb7173ae103b78ac0b47d402e27a
Esther-mac <esther_lin@mail.chungyo.net>
2020-08-28 11:22:46 +0800

    get weeks data

---
+++ public/GetCwb.php
@@ -0,0 +1,35 @@
+<?php 
+
+header("Content-Type:text/html; charset=utf-8");
+
+
+$json = file_get_contents('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-ED7050C5-A1A2-4409-B999-A4CCC6F4AFBB&elementName=MinT,MaxT,Wx,PoP12h');
+$data = json_decode($json,true);
+$data_location = $data['records']['locations'][0]['location'];
+
+
+foreach($data_location as $location_k => $location_v){
+    echo "location: ".$location_v['locationName'];
+    echo "<br>";
+    foreach($location_v['weatherElement'] as $name_k => $name_v){
+        echo "elementName: ".$name_v['elementName'];
+        echo "<hr>";
+            foreach($name_v['time'] as $time_k => $time_v){
+                echo $time_k;
+                echo $time_v['startTime']." ~ ".$time_v['endTime'];
+                echo'<br>';
+                foreach($time_v['elementValue'] as $value_k => $value_v){
+                    $value = intval($value_v['value']);
+                    echo ($value);         
+                    echo"<br>-----------------------<br>";                  
+                    
+                }
+                
+            }
+        
+    }
+    echo "<hr>";
+}
+
+
+?>
