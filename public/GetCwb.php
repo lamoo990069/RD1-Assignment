b6282a659206160ef0c3955dfecba355943f30af
Esther-mac <esther_lin@mail.chungyo.net>
2020-08-28 16:39:49 +0800

    add POD Class: data to db

--- public/GetCwb.php
+++ public/GetCwb.php
@@ -1,38 +1,59 @@
 <?php 
 
 header("Content-Type:text/html; charset=utf-8");
+require('POD_data_to_db.php');
 
-
+//get cwb_api data
 $json = file_get_contents('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-ED7050C5-A1A2-4409-B999-A4CCC6F4AFBB&elementName=MinT,MaxT,Wx,PoP12h');
 $data = json_decode($json,true);
 $data_location = $data['records']['locations'][0]['location'];
 
 
-foreach($data_location as $location_k => $location_v){
-    echo "location: ".$location_v['locationName'];
-    echo "<br>";
-    foreach($location_v['weatherElement'] as $name_k => $name_v){
-        echo "elementName: ".$name_v['elementName'];
-        echo "<hr>";
-            foreach($name_v['time'] as $time_k => $time_v){
-                echo $time_k;
-                echo $time_v['startTime']." ~ ".$time_v['endTime'];
-                echo'<br>';
-                foreach($time_v['elementValue'] as $value_k => $value_v){
-                    $value = $value_v['value'];
-                    if(is_numeric($value) || $value == " ")
-                        $value = intval($value_v['value']);
+$week_pod = new cwbPDO();
+
+
+//grab data
+function getWeatherWeek($data){
+
+    $list = [];
+
+    foreach($data as $location){
+
+        $list['location'] = $location['locationName'];
+        foreach($location['weatherElement'] as $name){
+
+            $list['elementName'] = $name['elementName'];
+            foreach($name['time'] as $time){
+
+                $list['startTime'] = $time['startTime'];
+
+                foreach($time['elementValue'] as $value){
+                    $v = $value['value'];
+                    if(is_numeric($v) || $v == " ")
+                        $v= intval($v);
                     
-                    echo ($value);         
-                    echo"<br>-----------------------<br>";                  
+                        $list['value'] = $v;
+
+                        //do upload here
+                        //insert_week($list);
+                        print_r($list);
+                        echo "<br>";
+                
                 break;
                 }
                 
             }
+            
+        }
+        echo "<hr>";
+        $list = array();
         
     }
-    echo "<hr>";
 }
+
+
+
+
 
 
 ?>
---
+++ public/POD_data_to_db.php
@@ -0,0 +1,93 @@
+<?php
+
+class cwbPDO extends PDO{
+    public static $dbtype = 'mysql';
+    public static $dbhost = 'localhost';
+    public static $dbport = 8889;
+    public static $dbname = 'RD1-Assignment';
+    public static $dbuser = 'root';
+    public static $dbpass = 'root';
+    public static $DB = null;
+    
+
+    public function __construct(){
+        self::connect();
+        //buffered: Client緩衝區
+        self::$DB->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+        //預處理
+        self::$DB->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+        self::execute('SET NAMES utf8');
+    }
+    public function __destruct(){
+        self::close();
+    }
+
+    //connect
+    public function connect(){
+        try{
+            self::$DB = new PDO(self::$dbtype . ':host=' . self::$dbhost . ';port=' . self::
+            $dbport . ';dbname=' . self::$dbname, self::$dbuser, self::$dbpass, array(PDO::
+            ATTR_PERSISTENT => true));
+        }
+        catch(PDOException $e){
+            die("Connect Error Infomation:" . $e->getMessage());
+        }
+    }
+    //close
+    public function close(){
+        self::$DB = null;
+    }
+
+    public function now() {
+        return date("Y-m-d H:i:s");
+    }
+
+    private function getCode($table, $args){
+        $code = '';
+        if(is_array($args)){
+            foreach ($args as $k => $v){
+                if($v == '')
+                    continue;
+                $code .="`$k` = '$v',";
+            }
+        }
+        $code = substr($code, 0, -1);
+        return $code;
+    }
+
+
+    //執行sql語句
+    public function execute($sql) {
+        return self::$DB->exec($sql);
+    }
+
+    // get: feth_row
+    public function fetRowCount($table, $field = '*', $where = false){
+        $sql = "SELECT COUNT({$field}) AS num FROM `$table`";
+        $sql .= ($where) ? "WHERE $where" : "";
+        
+        return (self::$DB->query($sql))->rowCount();
+    }
+
+    public function add($table, $args){
+        $sql = "INSERT INTO `$table` SET";
+        $sql .=self::getCode($table, $args);
+
+        return self::execute($sql);
+
+    }
+
+
+
+
+}
+
+// $data = [ 'location' => '雲林縣',
+//           'elementName' => 'PoP12h',
+//           'startTime' => '2020-08-28 12:00:00',
+//           'value' => '30'];
+
+// $week_pod = new cwbPDO();
+// echo $week_pod->add('weather_week',$data);
+
+?>
